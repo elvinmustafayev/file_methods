640e3f85b4d36fc928c01d6207263d6aadc68762
Mustafayev Elvin <elvinmustafadev@gmail.com>
2021-02-04 21:51:57 +0400

    changes

--- src/com/company/FileUtilityClass.java
+++ src/com/company/FileUtilityClass.java
@@ -1,4 +1,25 @@
 package com.company;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileUtilityClass {
+
+    public static void writeIntoFile(String fileName, String text) throws IOException {
+
+        try(
+                  BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+
+                 bw.write(text);
+        }
+
+
+
+
+
+
+
+    }
+
 }
